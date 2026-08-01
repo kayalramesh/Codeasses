01bd0b1bcdd5a99e66bed2fe3e1ab07077f974ee
kayalramesh <kayalramesh1902@gmail.com>
2026-08-01 23:15:03 +0530

    Update API URL

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:3001/api';
+const API_BASE = 'https://codeasses-api.up.railway.app/api';
 
 const fetchWithAuth = async (url, options = {}) => {
   const token = localStorage.getItem('token');
